84368e655c0c85c62467d48eebbfcab735a55def
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2025-08-07 21:13:28 +0200

    Update manifests in update_ebuilds.py

--- scripts/update_ebuilds.py
+++ scripts/update_ebuilds.py
@@ -3,7 +3,9 @@
 #
 # SPDX-License-Identifier: MIT
 
+import argparse
 import glob
+import hashlib
 import json
 import os
 import requests
@@ -11,6 +13,7 @@ import subprocess
 
 
 BRAVE_RELEASES = "https://api.github.com/repos/brave/brave-browser/releases"
+MANIFEST_HASH_ALGOS = ("BLAKE2B", "SHA512")
 
 
 def extract_version(path):
@@ -59,7 +62,8 @@ def get_new_releases(releases, repo_dir):
     for key, version in releases.items():
         suffix = "" if key == "stable" else f"-{key}"
         ebuild_path = os.path.join(
-            repo_dir, f"www-client/brave-browser{suffix}/brave-browser{suffix}-{version}.ebuild"
+            repo_dir,
+            f"www-client/brave-browser{suffix}/brave-browser{suffix}-{version}.ebuild",
         )
         if not os.path.exists(ebuild_path):
             new_releases[key] = releases[key]
@@ -92,6 +96,8 @@ def update_ebuilds(new_releases, repo_dir, commit_changes=False):
         new_ebuilds[key] = os.path.relpath(new_ebuild, repo_dir)
         added.append(f"{version}")
 
+        update_manifest(ebuild_dir)
+
         if commit_changes:
             message = ", ".join(
                 filter(
@@ -109,12 +115,85 @@ def update_ebuilds(new_releases, repo_dir, commit_changes=False):
             )
 
 
+def update_manifest(ebuild_dir):
+    ebuilds = glob.glob(os.path.join(ebuild_dir, "*.ebuild"))
+    versions = set(extract_version(ebuild) for ebuild in ebuilds)
+    versions_in_manifest = set()
+    sources = {
+        os.path.basename(source.url): source
+        for source in [
+            {
+                "url": f"https://github.com/brave/brave-browser/releases/download/v{version}/brave-browser_{version}_amd64.deb",
+                "version": version,
+            }
+            for version in versions
+        ]
+    }
+
+    with open(os.path.join(ebuild_dir, "Manifest"), "r") as f:
+        lines = f.readlines()
+        new_lines = []
+        for line in lines:
+            parts = line.split(" ")
+            if parts[0] == "DIST":
+                if parts[1] in sources.keys():
+                    # Keep DIST lines for current ebuilds
+                    new_lines.append(line)
+                    versions_in_manifest.append(sources[parts[1]]["version"])
+            else:
+                new_lines.append(line)
+
+    # Add DIST lines for new ebuilds
+    for version in versions - versions_in_manifest:
+        url = next(
+            (
+                source["url"]
+                for source in sources.values()
+                if source["version"] == version
+            ),
+            None,
+        )
+        assert url is not None
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        size = len(response.content)
+
+        def calc_hash(algo, data):
+            h = hashlib.new(algo.lower())
+            h.update(data)
+            return h.hexdigest()
+
+        hashes = [
+            (algo, calc_hash(algo, response.content)) for algo in MANIFEST_HASH_ALGOS
+        ]
+
+        new_lines.append(
+            f"DIST {os.path.basename(url)} {size} {[' '.join(hash) for hash in hashes]}\n"
+        )
+
+    with open(os.path.join(ebuild_dir, "Manifest"), "w") as f:
+        f.writelines(new_lines)
+
+
 def main():
+    parser = argparse.ArgumentParser(
+        description="Update ebuilds for Brave browser releases."
+    )
+    parser.add_argument(
+        "--commit",
+        action="store_true",
+        default=False,
+        help="Commit changes to the repository.",
+    )
+    args = parser.parse_args()
+
     repo_dir = os.join(os.dirname(__file__), "..")
 
     releases = get_latest_releases()
     new_releases = get_new_releases(releases, repo_dir)
-    new_ebuilds = update_ebuilds(new_releases, repo_dir)
+    new_ebuilds = update_ebuilds(new_releases, repo_dir, commit_changes=args.commit)
 
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f:
